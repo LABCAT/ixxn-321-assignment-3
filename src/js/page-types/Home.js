b82a5e9df7923055bcf48fd7aa130e2d87b56b1d
LABCAT <shane.watters.nz@gmail.com>
2020-06-10 13:48:57 +1200

    Moving Graph component into HomePage.

--- src/js/page-types/Home.js
+++ src/js/page-types/Home.js
@@ -2,6 +2,7 @@ import React, { useContext, useState, useCallback } from 'react';
 
 import { Context } from '../context/Context.js';
 import NZMap from '../components/NZMap.js';
+import Graph from '../components/Graph';
 import SearchForm from '../components/SearchForm.js';
 
 
@@ -23,19 +24,24 @@ export default function Home() {
             {
                 showMapSection ?
                 <div className="home-page-map-section">
-                    <NZMap />
-                    <dl>
-                        <dt>Current region selected</dt>
-                        <dd>{Region}</dd>
-                        <dt>Active Cases</dt>
-                        <dd>{Active}</dd>
-                        <dt>Recovered Cases</dt>
-                        <dd>{Recovered}</dd>
-                        <dt>Deceased</dt>
-                        <dd>{Deceased}</dd>
-                        <dt>Total Cases</dt>
-                        <dd>{Total}</dd>
-                    </dl>
+                    <div className="home-page-map-section-column">
+                        <NZMap />
+                    </div>
+                    <div className="home-page-map-section-column">
+                        <Graph />
+                        <dl>
+                            <dt>Current region selected</dt>
+                            <dd>{Region}</dd>
+                            <dt>Active Cases</dt>
+                            <dd>{Active}</dd>
+                            <dt>Recovered Cases</dt>
+                            <dd>{Recovered}</dd>
+                            <dt>Deceased</dt>
+                            <dd>{Deceased}</dd>
+                            <dt>Total Cases</dt>
+                            <dd>{Total}</dd>
+                        </dl>
+                    </div>
                 </div> :
                 <div className="home-page-search-section">
                     <SearchForm />

760e0016dd4c0e83c0c89a0a0e4c91d2f83b9ab5
dylanmarc <diloubilou@gmail.com>
2020-06-10 12:24:37 +1200

    first commit, started working on graph and styling

--- src/js/page-types/Home.js
+++ src/js/page-types/Home.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 
 import { Context } from '../context/Context.js';
 
+
 export default function Home() {
     const { alertLevel } = useContext(Context);
     return (

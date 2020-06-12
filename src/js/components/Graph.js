722378bd0157217b91500e997535c33dde5c1f6f
dylanmarc <diloubilou@gmail.com>
2020-06-12 13:31:18 +1200

    Made graph change based on clicked reigon on map, fixed stylingand added animation.

--- src/js/components/Graph.js
+++ src/js/components/Graph.js
@@ -1,55 +1,68 @@
-import React, { Component } from 'react'
-import { VictoryPie, VictoryLabel } from 'victory';
+import React, { useContext } from 'react'
+import { VictoryPie, VictoryLabel, VictoryContainer } from 'victory';
+import { Context } from '../context/Context.js';
 
 export default function Graph() {
+    const { currentRegion } = useContext(Context);
+    const { Region, Active, Recovered, Deceased, Total } = currentRegion;
+
     const data = [
-        {x: "", y: 265},
-        {x: "", y: 40}
+        {x: "", y: Recovered},
+        {x: "", y: Deceased}
     ]
+
+    function graphPad(number){
+        if(number == 0){
+            return 0
+        }else{
+            return 1
+        }
+    }
 
 
         return (
             <div>
-                <svg viewBox="0 0 250 250">
+                <svg viewBox="0 0 400 400">
                     <VictoryPie
+                        containerComponent={<VictoryContainer responsive={false}/>}
                         standalone={false}
-                        width={250} height={250}
-                        padAngle={3}
-                        innerRadius={100}
+                        width={400} height={400}
+                        padAngle={graphPad(Deceased)}
+                        innerRadius={130}
                         colorScale={['#A0FF90','#FF6767']}
                         labels={() => null}
+                        animate={{duration: 500}}
                         data={[
                             data[0],
                             data[1]
                         ]}
-                        
                     />
                     <VictoryLabel 
                         textAnchor="middle"
                         verticalAnchor="middle"
-                        x={125} y={90}
-                        style={{ fontSize: 50, fill: '#A0FF90', fontWeight: '600'}}
+                        x={200} y={130}
+                        style={{ fontSize: 40, fill: '#A0FF90', fontWeight: '600', fontFamily: 'Lato'}}
                         text={`${data[0].y}`}
                     />
                     <VictoryLabel 
                         textAnchor="middle"
                         verticalAnchor="middle"
-                        x={125} y={115}
-                        style={{ fill: '#A0FF90', fontSize: 16 }}
+                        x={200} y={155}
+                        style={{ fill: '#A0FF90', fontSize: 16, fontWeight: '100', fontFamily: 'Lato'}}
                         text={'Recovered'}
                     />
                     <VictoryLabel 
                         textAnchor="middle"
                         verticalAnchor="middle"
-                        x={125} y={155}
-                        style={{ fontSize: 50, fill: '#FF6767', fontWeight: '600'}}
+                        x={200} y={200}
+                        style={{ fontSize: 40, fill: '#FF6767', fontWeight: '600', fontFamily: 'Lato'}}
                         text={`${data[1].y}`}
                     />
                     <VictoryLabel 
                         textAnchor="middle"
                         verticalAnchor="middle"
-                        x={125} y={180}
-                        style={{fill: '#FF6767', fontSize: 16}}
+                        x={200} y={225}
+                        style={{fill: '#FF6767', fontSize: 16, fontWeight: '100', fontFamily: 'Lato'}}
                         text={'Infected'}
                     />
                 </svg>

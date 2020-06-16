8fdfa23617248b7dcdd8d6b1428cc51f40ae2931
dylanmarc <diloubilou@gmail.com>
2020-06-16 16:55:45 +1200

    fixed styling of pie graph, added and styled bar graph for mobile. Made them change depending on the width of the page (need to make it change automatically as it only works when switching pages)

--- src/js/page-types/Home.js
+++ src/js/page-types/Home.js
@@ -5,20 +5,43 @@ import { Transition } from 'react-spring/renderprops';
 
 import NZMap from '../components/NZMap.js';
 import Graph from '../components/Graph';
+import GraphMobile from '../components/GraphMobile';
 import SearchForm from '../components/SearchForm.js';
 
 import * as Constants from '../context/Constants.js';
 
 export default function Home() {
     const [showMapSection, setShowMapSection] = useState(false);
-    const handleClick = () => setShowMapSection(!showMapSection)
+    const handleClick = () => setShowMapSection(!showMapSection);
+
+    //code to change the graph once the page width is less than 1000px
+    const mq = window.matchMedia('(max-width: 1000px)');
+    function graphChange(mq) {
+        if (mq.matches) {
+            return (
+                <div style={{display:"flex", flexDirection:"column", alignItems:"center", justifyContent:"center"}}>
+
+                        <GraphMobile/>
+
+                        <NZMap/>
+
+                </div>)
+        } else {
+            return (
+                <div>
+                    <NZMap />
+                    <Graph />
+                </div>)
+        }
+    }
+
 
     return (
         <section className="home-page">
             <label className="home-page-switch">
-                <input 
-                    type="checkbox" 
-                    onClick={handleClick} 
+                <input
+                    type="checkbox"
+                    onClick={handleClick}
                 />
                 <span className="home-page-slider round"></span>
             </label>
@@ -29,41 +52,40 @@ export default function Home() {
                 leave={{ opacity: 0, transform: 'translateX(-2000px)' }}>
                 {showMapSection =>
                     showMapSection
-                        ? props => 
+                        ? props =>
                             <div className="home-page-map-section" style={props}>
-                                <div className="home-page-map-section-column" style={{width:"80%", marginLeft:'auto', marginRight:'auto'}}>
-                                    <NZMap />
-                                    <Graph />
+                                <div className="home-page-map-section-column" style={{ width: "70%", marginLeft: 'auto', marginRight: 'auto' }}>
+                                    {graphChange(mq)}
                                 </div>
                             </div>
-                        : props => 
-                        <div className="home-page-search-section" style={props}>
-                            <SearchForm />
-                            <div className="home-page-search-section-quick-links">
-                                {
-                                    Constants.categories.filter(
-                                        function (category) {
-                                            if (category.value === "All") {
-                                                return false; 
+                        : props =>
+                            <div className="home-page-search-section" style={props}>
+                                <SearchForm />
+                                <div className="home-page-search-section-quick-links">
+                                    {
+                                        Constants.categories.filter(
+                                            function (category) {
+                                                if (category.value === "All") {
+                                                    return false;
+                                                }
+                                                return true;
                                             }
-                                            return true;
-                                        }
-                                    ).map(
-                                        category => (
-                                            <Link to={`/search/category=${category.title}`}>
-                                                <div className="image-holder">
-                                                    <img
-                                                        src={require(`../../images/${category.title}.png`)}
-                                                        alt={category.title}
-                                                    />
-                                                </div>
-                                                <h2>{category.title}</h2>
-                                            </Link>
+                                        ).map(
+                                            category => (
+                                                <Link to={`/search/category=${category.title}`}>
+                                                    <div className="image-holder">
+                                                        <img
+                                                            src={require(`../../images/${category.title}.png`)}
+                                                            alt={category.title}
+                                                        />
+                                                    </div>
+                                                    <h2>{category.title}</h2>
+                                                </Link>
+                                            )
                                         )
-                                    )
-                                }
+                                    }
+                                </div>
                             </div>
-                        </div>
                 }
             </Transition>
         </section>

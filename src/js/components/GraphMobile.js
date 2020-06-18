5fd7419f010db37e3f22e2e26078722106dcb63a
dylanmarc <diloubilou@gmail.com>
2020-06-18 16:20:46 +1200

    fixed responsiveness for mobile-graph, fixed page-slider centering

--- src/js/components/GraphMobile.js
+++ src/js/components/GraphMobile.js
@@ -26,7 +26,7 @@ export default function Graph() {
                         textAnchor="middle"
                         verticalAnchor="middle"
                         x={500} y={220}
-                        style={{ fill: '#005D70', fontSize: 26, fontWeight: '100', fontFamily: 'Lato' }}
+                        style={{ fill: '#005D70', fontSize: 36, fontWeight: '100', fontFamily: 'Lato' }}
                         text={'Reset to Nationwide'}
                     />
                 </a>
@@ -37,7 +37,7 @@ export default function Graph() {
     }
 
         return (
-            <svg width="100%" height="100%" viewBox="0 90 1000 250">
+            <svg width="100%" height="100%" viewBox="0 70 1000 250">
                 <VictoryStack
                 standalone={false}
                 colorScale={['#A0FF90', '#FF6767']}
@@ -47,47 +47,47 @@ export default function Graph() {
                 >
                 <VictoryBar
                     data={[data[0]]}
-                    barWidth={60}
+                    barWidth={75}
                 />
                 <VictoryBar
                     data={[data[1]]}
-                    barWidth={60}
+                    barWidth={75}
                 />
                 </VictoryStack>
 
                 <VictoryLabel
                     textAnchor="middle"
                     verticalAnchor="middle"                    
-                    x={500} y={100}
-                    style={{ fill: '#FFFFFF', fontSize: 26, fontWeight: '100', fontFamily: 'Lato' }}
+                    x={500} y={95}
+                    style={{ fill: '#FFFFFF', fontSize: 36, fontWeight: '100', fontFamily: 'Lato' }}
                     text={`${data[2].name}`}
                 />
                 <VictoryLabel
                     textAnchor="middle"
                     verticalAnchor="middle"
-                    x={150} y={220}
-                    style={{ fontSize: 60, fill: '#A0FF90', fontWeight: '600', fontFamily: 'Lato' }}
+                    x={150} y={230}
+                    style={{ fontSize: 75, fill: '#A0FF90', fontWeight: '750', fontFamily: 'Lato' }}
                     text={`${data[0].y}`}
                 />
                 <VictoryLabel
                     textAnchor="middle"
                     verticalAnchor="middle"
-                    x={150} y={255}
-                    style={{ fill: '#A0FF90', fontSize: 26, fontWeight: '100', fontFamily: 'Lato' }}
+                    x={150} y={275}
+                    style={{ fill: '#A0FF90', fontSize: 36, fontWeight: '100', fontFamily: 'Lato' }}
                     text={'Recovered'}
                 />
                 <VictoryLabel
                     textAnchor="middle"
                     verticalAnchor="middle"
-                    x={850} y={220}
-                    style={{ fontSize: 60, fill: '#FF6767', fontWeight: '600', fontFamily: 'Lato' }}
+                    x={850} y={230}
+                    style={{ fontSize: 75, fill: '#FF6767', fontWeight: '750', fontFamily: 'Lato' }}
                     text={`${data[1].y}`}
                 />
                 <VictoryLabel
                     textAnchor="middle"
                     verticalAnchor="middle"
-                    x={850} y={255}
-                    style={{ fill: '#FF6767', fontSize: 26, fontWeight: '100', fontFamily: 'Lato' }}
+                    x={850} y={275}
+                    style={{ fill: '#FF6767', fontSize: 36, fontWeight: '100', fontFamily: 'Lato' }}
                     text={'Deceased'}
                 />
                 {resetButton(data[2].name)}

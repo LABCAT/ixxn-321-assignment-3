bcf028fb62e6fc8464b426d19d13afb0a4cf8b15
LABCAT <shane.watters.nz@gmail.com>
2020-06-25 18:17:27 +1200

    Resolving error preventing build on netlify.

--- src/js/page-types/Home.js
+++ src/js/page-types/Home.js
@@ -1,5 +1,5 @@
-import React, { useContext, useState, useEffect } from 'react';
-import { useHistory } from "react-router-dom";
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
 import { Transition } from 'react-spring/renderprops';
 
 
@@ -8,27 +8,12 @@ import Graph from '../components/Graph';
 import GraphMobile from '../components/GraphMobile';
 import SearchForm from '../components/SearchForm.js';
 
-import { Context } from '../context/Context.js';
 import * as Constants from '../context/Constants.js';
 
 export default function Home() {
-    let history = useHistory();
-    const { currentSearchQuery, setSearchQuery } = useContext(Context);
     const [showMapSection, setShowMapSection] = useState(false);
+    const handleClick = () => setShowMapSection(!showMapSection);
 
-    const handleClick = () =>  setShowMapSection(!showMapSection);
-
-    const handleCategoryClick = (category) => {
-
-        setSearchQuery(
-            {
-                "keyword": currentSearchQuery.keyword,
-                "category": category ? category : 'All'
-            }
-        );
-        const searchRoute = '/search/keyword=' + encodeURI(currentSearchQuery.keyword) + '/category=' + encodeURI(category ? category : 'All');
-        history.push(searchRoute);
-    };
 
     return (
         <section className="home-page">
@@ -39,59 +24,56 @@ export default function Home() {
                 />
                 <span className="home-page-slider round"></span>
             </label>
-            <div className="home-page-transition-holder" id="transition-holder">
-                <Transition
-                    items={showMapSection}
-                    from={{ opacity: 1, transform: 'translateX(2000px)', position: 'absolute' }}
-                    enter={{ opacity: 1, transform: 'translateX(0px)', position: 'relative' }}
-                    leave={{ opacity: 1, transform: 'translateX(-2000px)', position: 'absolute' }}
-                >
-                    {showMapSection =>
-                        showMapSection
-                            ? props =>
-                                <div className="home-page-map-section" id="map-section" style={props}>
-                                    <div className="home-page-map-section-column">
-                                        <div className="mobile-graph">
-                                            <GraphMobile />
-                                            <NZMap />
-                                        </div>
-                                        <div className="big-graph">
-                                            <NZMap />
-                                            <Graph />
-                                        </div>
+            <Transition
+                items={showMapSection}
+                from={{ opacity: 0, transform: 'translateX(2000px)' }}
+                enter={{ opacity: 1, transform: 'translateX(0px)' }}
+                leave={{ opacity: 0, transform: 'translateX(-2000px)' }}>
+                {showMapSection =>
+                    showMapSection
+                        ? props =>
+                            <div className="home-page-map-section" style={props}>
+                                <div className="home-page-map-section-column">
+                                    <div class="mobile-graph">
+                                        <GraphMobile />
+                                        <NZMap />
+                                    </div>
+                                    <div class="big-graph">
+                                        <NZMap />
+                                        <Graph />
                                     </div>
                                 </div>
-                            : props =>
-                                <div className="home-page-search-section" id="search-section" style={props}>
-                                    <SearchForm />
-                                    <div className="home-page-search-section-quick-links">
-                                        {
-                                            Constants.categories.filter(
-                                                function (category) {
-                                                    if (category.value === "All") {
-                                                        return false;
-                                                    }
-                                                    return true;
+                            </div>
+                        : props =>
+                            <div className="home-page-search-section" style={props}>
+                                <SearchForm />
+                                <div className="home-page-search-section-quick-links">
+                                    {
+                                        Constants.categories.filter(
+                                            function (category) {
+                                                if (category.value === "All") {
+                                                    return false;
                                                 }
-                                            ).map(
-                                                category => (
-                                                    <button onClick={() => handleCategoryClick(category.title)} key={category.title}>
-                                                        <div className="image-holder">
-                                                            <img
-                                                                src={require(`../../images/${category.title}.png`)}
-                                                                alt={category.title}
-                                                            />
-                                                        </div>
-                                                        <h2>{category.title}</h2>
-                                                    </button>
-                                                )
+                                                return true;
+                                            }
+                                        ).map(
+                                            category => (
+                                                <Link to={`/search/category=${category.title}`}>
+                                                    <div className="image-holder">
+                                                        <img
+                                                            src={require(`../../images/${category.title}.png`)}
+                                                            alt={category.title}
+                                                        />
+                                                    </div>
+                                                    <h2>{category.title}</h2>
+                                                </Link>
                                             )
-                                        }
-                                    </div>
+                                        )
+                                    }
                                 </div>
-                    }
-                </Transition>
-            </div>
+                            </div>
+                }
+            </Transition>
         </section>
     );
 }

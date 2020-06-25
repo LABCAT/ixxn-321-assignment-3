93b08458961abb015b9699f72df6551bc9106eeb
LABCAT <shane.watters.nz@gmail.com>
2020-06-25 17:26:06 +1200

    Adding ScrollToTop function to ensure user is always taken to the top of the viewport on route change.

--- src/js/layout/Main.js
+++ src/js/layout/Main.js
@@ -13,15 +13,23 @@ export default function Main() {
     return (
         <main className='main'>
             <Switch>
-                <Route exact path="/" component={Home} />
-                <Route path="/search" component={SearchResultsPage} />
-                <Route  path="/page/:id" component={Page} />
-                <Route exact path="/style-guide" component={StyleGuide} />
-
-                <Route path="/info" component={Info} />
-
+                <ScrollToTop path="*">
+                    <Route exact path="/" component={Home} />
+                    <Route path="/search" component={SearchResultsPage} />
+                    <Route path="/page/:id" component={Page} />
+                    <Route exact path="/style-guide" component={StyleGuide} />
+                    <Route path="/info" component={Info} />
+                </ScrollToTop>
             </Switch>
         </main>
         
     );
+}
+
+/* these two functions should really be moved to their own files, even though this is the only component they are used in. */
+
+export const ScrollToTop = ({ children, location }) => {
+    //ensures that the scroll position will always begin at the top when a route changes 
+    React.useLayoutEffect(() => window.scrollTo(0, 0), [location.pathname]);
+    return children;
 }
